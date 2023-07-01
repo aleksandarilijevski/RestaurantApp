cefd3072c8d1ed5a52fbe7c4f3bdefc94b02bcfb
ilijevskialeksandar@hotmail.com <ilijevskialeksandar@hotmail.com>
2023-07-01 21:26:27 +0200

    Barcode and entryPrice attributes are added to the "Artical" model.

---
+++ EntityFramework/Migrations/20230701192538_ArticalModelModified.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArticalModelModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "BarCode",
+                table: "Articals",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "EntryPrice",
+                table: "Articals",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BarCode",
+                table: "Articals");
+
+            migrationBuilder.DropColumn(
+                name: "EntryPrice",
+                table: "Articals");
+        }
+    }
+}
--- EntityFramework/Models/Artical.cs
+++ EntityFramework/Models/Artical.cs
@@ -4,11 +4,15 @@
     {
         public int ID { get; set; }
 
+        public long BarCode { get;set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public int Quantity { get; set; }
+
+        public decimal EntryPrice { get; set; }
 
         public decimal Price { get; set; }
     }
