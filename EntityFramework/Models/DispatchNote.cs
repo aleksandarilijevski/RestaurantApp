9af51fd63c435782c6bb2f72b06f6e2a8809bfdf
ilijevskialeksandar@hotmail.com <ilijevskialeksandar@hotmail.com>
2023-07-17 13:38:13 +0200

    DispatchNote model is added

---
+++ EntityFramework/Models/DispatchNote.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework.Models
+{
+    public class DispatchNote
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int DispatchNoteNumber { get; set; }
+
+        public List<Article> Articles { get; set; }
+
+        public decimal TotalAmount { get; set; }
+    }
+}
