1748283dbcf3224b2f06d862fa57e86c598aebd5
ilijevskialeksandar@hotmail.com <ilijevskialeksandar@hotmail.com>
2023-07-22 19:54:41 +0200

    Converter for calculating quantity is implemented

---
+++ RestaurantApp/Converters/TotalQuantityConverter.cs
@@ -0,0 +1,27 @@
+﻿using EntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace RestaurantApp.Converters
+{
+    public class TotalQuantityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is List<ArticleDetails> articleDetails)
+            {
+                return articleDetails.Sum(details => details.Quantity);
+            }
+
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
