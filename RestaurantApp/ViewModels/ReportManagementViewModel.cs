3c178c737e093e7c1e4955668c1fe3c6ec33ce0e
ilijevskialeksandar@hotmail.com <ilijevskialeksandar@hotmail.com>
2023-09-08 12:43:11 +0200

    Exporting excel file doesn't rely if office is installed on computer or no.
    We are using ClosedXML nuget package since it's free.
    
    ChatGPT converted old code to new code to use ClosedXML nuget. Correction was necessery.

--- RestaurantApp/ViewModels/ReportManagementViewModel.cs
+++ RestaurantApp/ViewModels/ReportManagementViewModel.cs
@@ -1,4 +1,5 @@
-﻿using EntityFramework.Models;
+﻿using ClosedXML.Excel;
+using EntityFramework.Models;
 using Microsoft.Win32;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -9,9 +10,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Windows;
-using Excel = Microsoft.Office.Interop.Excel;
+//using Excel = Microsoft.Office.Interop.Excel;
 
 namespace RestaurantApp.ViewModels
 {
@@ -285,6 +285,9 @@ namespace RestaurantApp.ViewModels
             TotalProfit = "Total profit : " + totalProfit.ToString("0.00");
         }
 
+        /// <summary>
+        /// Problem working on different excel version due to com reference.
+        /// </summary>
         private void ExportToExcel()
         {
             if (Bills.Count == 0)
@@ -297,9 +300,9 @@ namespace RestaurantApp.ViewModels
 
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.FileName = defaultFileName;
-            saveFileDialog.DefaultExt = ".xls";
+            saveFileDialog.DefaultExt = ".xlsx";
             saveFileDialog.AddExtension = true;
-            saveFileDialog.Filter = "Microsoft Excel (.xls)|*.xls";
+            saveFileDialog.Filter = "Microsoft Excel (.xlsx)|*.xlsx";
             bool? result = saveFileDialog.ShowDialog();
 
             if (result == false)
@@ -309,45 +312,137 @@ namespace RestaurantApp.ViewModels
 
             string fileLocation = saveFileDialog.FileName;
 
-            if (Path.GetExtension(fileLocation) != ".xls")
+            if (Path.GetExtension(fileLocation) != ".xlsx")
             {
                 MessageBox.Show("Invalid format!", "Export to excel", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
 
-            Excel.Application excel = new Excel.Application();
-            Excel.Workbook workbook = (Excel.Workbook)(excel.Workbooks.Add(Missing.Value));
-            Excel.Worksheet worksheet = (Excel.Worksheet)workbook.ActiveSheet;
+            //Excel.Application excel = new Excel.Application();
+            //Excel.Workbook workbook = (Excel.Workbook)(excel.Workbooks.Add(Missing.Value));
+            //Excel.Worksheet worksheet = (Excel.Worksheet)workbook.ActiveSheet;
 
-            worksheet.Columns[1].ColumnWidth = 20;
-            worksheet.Columns[2].ColumnWidth = 15;
-            worksheet.Columns[3].ColumnWidth = 15;
-            worksheet.Columns[4].ColumnWidth = 15;
-            worksheet.Columns[5].ColumnWidth = 20;
+            //worksheet.Columns[1].ColumnWidth = 20;
+            //worksheet.Columns[2].ColumnWidth = 15;
+            //worksheet.Columns[3].ColumnWidth = 15;
+            //worksheet.Columns[4].ColumnWidth = 15;
+            //worksheet.Columns[5].ColumnWidth = 20;
+
+            //int cellIndex = 1;
+
+            //foreach (Bill bill in Bills)
+            //{
+            //    List<TableArticleQuantity> soldTableArticleQuantities = bill.Table.TableArticleQuantities.Where(x => (x is SoldTableArticleQuantity)).ToList();
+
+            //    worksheet.Cells[cellIndex, 1] = "Table ID";
+            //    worksheet.Cells[cellIndex, 2] = "Payment type";
+            //    worksheet.Cells[cellIndex, 3] = "Total price";
+            //    worksheet.Cells[cellIndex, 4] = "Total profit";
+            //    worksheet.Cells[cellIndex, 5] = "Created date";
+
+            //    worksheet.Cells[cellIndex, 1].EntireRow.Font.Bold = true;
+
+            //    cellIndex++;
+
+            //    worksheet.Cells[cellIndex, 1] = bill.TableID;
+            //    worksheet.Cells[cellIndex, 2] = bill.PaymentType;
+            //    worksheet.Cells[cellIndex, 3] = bill.TotalPrice;
+
+            //    decimal profit = 0;
+
+            //    List<TableArticleQuantity> filteredSoldTableArticleQuantities = soldTableArticleQuantities.Where(x => x.BillID == bill.ID).ToList();
+
+            //    foreach (SoldTableArticleQuantity soldTableArticleQuantity in filteredSoldTableArticleQuantities)
+            //    {
+            //        int quantity = filteredSoldTableArticleQuantities.Sum(x => x.Quantity);
+
+            //        foreach (ArticleDetails articleDetail in soldTableArticleQuantity.ArticleDetails)
+            //        {
+            //            profit = bill.TotalPrice - (quantity * articleDetail.EntryPrice);
+            //        }
+            //    }
+
+            //    worksheet.Cells[cellIndex, 4] = profit;
+            //    worksheet.Cells[cellIndex, 5] = bill.CreatedDateTime;
+
+            //    cellIndex++;
+            //    worksheet.Cells[cellIndex, 1] = "Article name";
+            //    worksheet.Cells[cellIndex, 2] = "Article price";
+            //    worksheet.Cells[cellIndex, 3] = "Sold quantity";
+            //    worksheet.Cells[cellIndex, 4] = "Total price";
+            //    worksheet.Cells[cellIndex, 1].EntireRow.Font.Bold = true;
+
+            //    cellIndex++;
+
+            //    foreach (SoldTableArticleQuantity soldTableArticleQuantity in soldTableArticleQuantities)
+            //    {
+            //        if (soldTableArticleQuantity.BillID == bill.ID)
+            //        {
+            //            worksheet.Cells[cellIndex, 1] = soldTableArticleQuantity.Article.Name;
+            //            worksheet.Cells[cellIndex, 2] = soldTableArticleQuantity.Article.Price;
+            //            worksheet.Cells[cellIndex, 3] = soldTableArticleQuantity.Quantity;
+            //            worksheet.Cells[cellIndex, 4] = soldTableArticleQuantity.Quantity * soldTableArticleQuantity.Article.Price;
+            //            cellIndex++;
+            //        }
+            //    }
+
+            //    cellIndex += 3;
+            //    worksheet.Cells[cellIndex] = string.Empty;
+            //}
+
+            //decimal totalProfit = CalculateTotalProfit();
+            //worksheet.Cells[cellIndex, 1].EntireRow.Font.Bold = true;
+            //worksheet.Cells[cellIndex, 1] = "Total profit";
+            //worksheet.Cells[cellIndex, 2] = totalProfit;
+
+            //workbook.SaveAs(fileLocation, Excel.XlFileFormat.xlWorkbookNormal);
+
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Sheet1");
+
+            // Set column widths
+            worksheet.Column(1).Width = 20;
+            worksheet.Column(2).Width = 15;
+            worksheet.Column(3).Width = 15;
+            worksheet.Column(4).Width = 15;
+            worksheet.Column(5).Width = 20;
 
             int cellIndex = 1;
 
+            // Loop through your bills (assuming you have a List<Bill> named Bills)
             foreach (Bill bill in Bills)
             {
-                List<TableArticleQuantity> soldTableArticleQuantities = bill.Table.TableArticleQuantities.Where(x => (x is SoldTableArticleQuantity)).ToList();
+                // Assuming List<TableArticleQuantity> soldTableArticleQuantities = bill.Table.TableArticleQuantities.ToList();
 
-                worksheet.Cells[cellIndex, 1] = "Table ID";
-                worksheet.Cells[cellIndex, 2] = "Payment type";
-                worksheet.Cells[cellIndex, 3] = "Total price";
-                worksheet.Cells[cellIndex, 4] = "Total profit";
-                worksheet.Cells[cellIndex, 5] = "Created date";
+                List<SoldTableArticleQuantity> soldTableArticleQuantities = null;
 
-                worksheet.Cells[cellIndex, 1].EntireRow.Font.Bold = true;
+                if (bill.Table is not null)
+                {
+                    soldTableArticleQuantities = bill.Table.TableArticleQuantities.OfType<SoldTableArticleQuantity>().ToList();
+                }
 
+                if (bill.OnlineOrder is not null)
+                {
+                    soldTableArticleQuantities = bill.OnlineOrder.TableArticleQuantities.OfType<SoldTableArticleQuantity>().ToList();
+                }
+
+                // Headers
+                worksheet.Cell(cellIndex, 1).Value = "Table ID";
+                worksheet.Cell(cellIndex, 2).Value = "Payment type";
+                worksheet.Cell(cellIndex, 3).Value = "Total price";
+                worksheet.Cell(cellIndex, 4).Value = "Total profit";
+                worksheet.Cell(cellIndex, 5).Value = "Created date";
+                worksheet.Row(cellIndex).Style.Font.Bold = true;
                 cellIndex++;
 
-                worksheet.Cells[cellIndex, 1] = bill.TableID;
-                worksheet.Cells[cellIndex, 2] = bill.PaymentType;
-                worksheet.Cells[cellIndex, 3] = bill.TotalPrice;
+                // Bill details
+                worksheet.Cell(cellIndex, 1).Value = bill.TableID;
+                worksheet.Cell(cellIndex, 2).Value = bill.PaymentType.ToString();
+                worksheet.Cell(cellIndex, 3).Value = bill.TotalPrice;
 
                 decimal profit = 0;
 
-                List<TableArticleQuantity> filteredSoldTableArticleQuantities = soldTableArticleQuantities.Where(x => x.BillID == bill.ID).ToList();
+                var filteredSoldTableArticleQuantities = soldTableArticleQuantities.Where(x => x.BillID == bill.ID).ToList();
 
                 foreach (SoldTableArticleQuantity soldTableArticleQuantity in filteredSoldTableArticleQuantities)
                 {
@@ -359,40 +454,41 @@ namespace RestaurantApp.ViewModels
                     }
                 }
 
-                worksheet.Cells[cellIndex, 4] = profit;
-                worksheet.Cells[cellIndex, 5] = bill.CreatedDateTime;
-
+                worksheet.Cell(cellIndex, 4).Value = profit;
+                worksheet.Cell(cellIndex, 5).Value = bill.CreatedDateTime;
                 cellIndex++;
-                worksheet.Cells[cellIndex, 1] = "Article name";
-                worksheet.Cells[cellIndex, 2] = "Article price";
-                worksheet.Cells[cellIndex, 3] = "Sold quantity";
-                worksheet.Cells[cellIndex, 4] = "Total price";
-                worksheet.Cells[cellIndex, 1].EntireRow.Font.Bold = true;
 
+                // Article details
+                worksheet.Cell(cellIndex, 1).Value = "Article name";
+                worksheet.Cell(cellIndex, 2).Value = "Article price";
+                worksheet.Cell(cellIndex, 3).Value = "Sold quantity";
+                worksheet.Cell(cellIndex, 4).Value = "Total price";
+                worksheet.Row(cellIndex).Style.Font.Bold = true;
                 cellIndex++;
 
                 foreach (SoldTableArticleQuantity soldTableArticleQuantity in soldTableArticleQuantities)
                 {
                     if (soldTableArticleQuantity.BillID == bill.ID)
                     {
-                        worksheet.Cells[cellIndex, 1] = soldTableArticleQuantity.Article.Name;
-                        worksheet.Cells[cellIndex, 2] = soldTableArticleQuantity.Article.Price;
-                        worksheet.Cells[cellIndex, 3] = soldTableArticleQuantity.Quantity;
-                        worksheet.Cells[cellIndex, 4] = soldTableArticleQuantity.Quantity * soldTableArticleQuantity.Article.Price;
+                        worksheet.Cell(cellIndex, 1).Value = soldTableArticleQuantity.Article.Name;
+                        worksheet.Cell(cellIndex, 2).Value = soldTableArticleQuantity.Article.Price;
+                        worksheet.Cell(cellIndex, 3).Value = soldTableArticleQuantity.Quantity;
+                        worksheet.Cell(cellIndex, 4).Value = soldTableArticleQuantity.Quantity * soldTableArticleQuantity.Article.Price;
                         cellIndex++;
                     }
                 }
 
                 cellIndex += 3;
-                worksheet.Cells[cellIndex] = string.Empty;
+                worksheet.Cell(cellIndex, 1).Value = string.Empty;
             }
 
             decimal totalProfit = CalculateTotalProfit();
-            worksheet.Cells[cellIndex, 1].EntireRow.Font.Bold = true;
-            worksheet.Cells[cellIndex, 1] = "Total profit";
-            worksheet.Cells[cellIndex, 2] = totalProfit;
+            worksheet.Row(cellIndex).Style.Font.Bold = true;
+            worksheet.Cell(cellIndex, 1).Value = "Total profit";
+            worksheet.Cell(cellIndex, 2).Value = totalProfit;
 
-            workbook.SaveAs(fileLocation, Excel.XlFileFormat.xlWorkbookNormal);
+            // Save the workbook to a file location
+            workbook.SaveAs(fileLocation);
         }
 
         private decimal CalculateTotalProfit()
