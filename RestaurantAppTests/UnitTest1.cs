cf71150aa50ff53b032740fb0d6cfb2596b4988b
ilijevskialeksandar@hotmail.com <ilijevskialeksandar@hotmail.com>
2023-09-23 01:51:35 +0200

    NUnit project is added.

---
+++ RestaurantAppTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RestaurantAppTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
